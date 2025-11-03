44f4f9c9340298c0e8b32d0fd504494327250e4d
Omar Amer <oiamer4778@gmail.com>
2025-11-03 08:46:26 +0200

    feat: added the GetLast method to IDatabase interface.

--- Message Processing and Anomaly Detection Service/DatabaseInstances/IDatabase.cs
+++ Message Processing and Anomaly Detection Service/DatabaseInstances/IDatabase.cs
@@ -3,4 +3,5 @@ namespace Message_Processing_and_Anomaly_Detection_Service.DatabaseInstances;
 public interface IDatabase
 {
     public Task SaveStatisticsAsync(ServerStatistics stats);
+    public Task<ServerStatistics> GetLast(string serverIdentifier);
 }
