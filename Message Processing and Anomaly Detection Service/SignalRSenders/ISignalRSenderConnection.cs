9e9e453543faae7acf679f0aaa062b4bb99e55fd
Omar Amer <oiamer4778@gmail.com>
2025-11-04 11:14:24 +0200

    refactor: renamed InvokeAsync to SendAsync in ISignalRSenderConnection.

--- Message Processing and Anomaly Detection Service/SignalRSenders/ISignalRSenderConnection.cs
+++ Message Processing and Anomaly Detection Service/SignalRSenders/ISignalRSenderConnection.cs
@@ -3,5 +3,5 @@ namespace Message_Processing_and_Anomaly_Detection_Service.SignalRSenders;
 public interface ISignalRSenderConnection
 {
     public Task StartAsync();
-    public Task InvokeAsync(string remoteProcedure, string message);
+    public Task SendAsync(string message);
 }
